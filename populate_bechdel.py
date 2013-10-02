b3ccb5a107d85f8f2eae220c30279263439f7e2c
Shannon Turner <shannon.v.turner@gmail.com>
2013-10-02 07:12:45 -0400

    Added populater
    Added script that checks the Bechdel Test API for all movie IDs and adds
    them to the database.
    
    There were lots of minor errors I encountered that were caught by the
    overbroad 'except Exception', which over time I plan to squash and
    update this script accordingly.

---
+++ populate_bechdel.py
@@ -0,0 +1,34 @@
+import bechdel
+import requests
+import time
+
+" Pre-populate database with everything currently in the Bechdel Test API "
+
+endpoint = 'http://bechdeltest.com/api/v1/getAllMovieIds'
+response = requests.get(endpoint).json()
+
+print len(response), " movies to cycle through."
+
+for movie in response:
+
+    time.sleep(1)
+
+    print "Now on ", movie['imdbid']
+
+    try:
+
+        # Bechdel Results
+        movie_response = requests.get('http://bechdeltest.com/api/v1/getMovieByImdbId?imdbid={0}'.format(movie['imdbid'])).json()
+
+        if 'tt' not in movie_response['imdbid']:
+            movie_response['imdbid'] = 'tt{0}'.format(movie_response['imdbid'])
+                                                      
+        bechdel.update_database(movie_response)
+
+        # OMDB / Tomato Results
+        omdb_response = requests.get('http://www.omdbapi.com/?i=tt{0}&y={1}&tomatoes=true'.format(movie['imdbid'], movie_response['year'])).json()
+        bechdel.update_database(omdb_response)
+
+    except Exception, e:
+        print "[ERROR] Failed on ", e, " continuing ..."
+                                 
